bd4add9f374e54c76ae60b55e7e5199515320b6b
manikia <monikaangel.ma15@gmail.com>
2022-12-22 00:29:59 -0800

    ntoes

--- redo/redo5.py
+++ redo/redo5.py
@@ -7,4 +7,17 @@
 
 #different type of separators: comma-separated value which is separated by a comma, character-separated value which is separated by a comma, semicolon or tab, a delimiter-separated valyue which destincts a row or rows in a table, or a tab-separated value which is a type of dsv and its formatted as a table when in the text file 
 
+#relational database: contains multiple data tables that relate to each other in a certain way 
+#flat file database: a flat file corresponds to a single table from the relational database, it denotes a relational database composed of a single data table
 
+filename = 'source.txt'
+file = open(filename, mode = 'r')
+#print(file.read()) #we do this if we want to format to read the details in the text but it will only print once, it wont print again. to avoid this we have to initialize the read to another variable. also but if we do just print(file) then it will return a wrapper
+fileText = file.read()
+print(fileText)
+print(fileText)
+
+#then we close the file bc if we dont we can lose control of the files status and we might get hacked
+print(file.closed) #will return if its closed
+file.close()
+print(file.closed)
