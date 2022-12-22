a6f30efaa98c23d2bc0d72c430ba9caf225509e8
Monika <monikaangel.ma15@gmail.com>
2022-12-21 17:58:53 -0800

    added notes

--- redo/redo5.py
+++ redo/redo5.py
@@ -2,6 +2,9 @@
 # text files are also known as flat files or ascii files
 #structured data is like an excel file, semi structured is like emails because it still have specified tags but its a little unstructured and then instructured data is no torganized and its like having a foledr with audio files or images: all of these combined is what is called big data
 
+#plain files: each data is tored as a separate line so we dont need separators
+#flat files: each data value is separated by a separator
 
+#different type of separators: comma-separated value which is separated by a comma, character-separated value which is separated by a comma, semicolon or tab, a delimiter-separated valyue which destincts a row or rows in a table, or a tab-separated value which is a type of dsv and its formatted as a table when in the text file 
 
 
