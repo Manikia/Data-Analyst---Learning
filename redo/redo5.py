226c80e43f97a81d8730b08c71badadefce5c850
Monika <monikaangel.ma15@gmail.com>
2022-12-23 15:51:51 -0800

    continues course and organized files

--- redo/redo5.py
+++ redo/redo5.py
@@ -10,7 +10,9 @@
 #relational database: contains multiple data tables that relate to each other in a certain way 
 #flat file database: a flat file corresponds to a single table from the relational database, it denotes a relational database composed of a single data table
 
-filename = 'source.txt'
+# fixed width datafiles are when a file and its columns have a set width so add informastion and cant be more or less than what intended/set
+
+filename = './redo/source.txt'
 file = open(filename, mode = 'r')
 #print(file.read()) #we do this if we want to format to read the details in the text but it will only print once, it wont print again. to avoid this we have to initialize the read to another variable. also but if we do just print(file) then it will return a wrapper
 fileText = file.read()
@@ -21,3 +23,23 @@ print(fileText)
 print(file.closed) #will return if its closed
 file.close()
 print(file.closed)
+
+#character meaning when opening a file: r:reading, w:writing, x:create a new file and open for writing, a:open for writing and append to the end of file if exists, t:text mode
+
+#its better to use a with method when opening a file, so instead of doing what we did above we can cut it down adn do this: but as before we have to set it to another variable or it wont print
+
+#with statement is better because it closes the file but the other method doesnt close it and we might miss closing it
+
+with open(filename, mode = "r") as output:
+    out_put = output.read()
+    print(out_put)
+    print(out_put)
+
+with open(filename, "w") as writing:
+    writing.write("Adding new information")
+    
+with open(filename, mode = "r") as newoutput:
+    new_out_put = newoutput.read()
+    print(new_out_put)
+    print(new_out_put)
+    
